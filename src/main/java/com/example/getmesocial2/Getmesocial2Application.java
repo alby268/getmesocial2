8485a66872035f5a9819661c4abef08bb195c71f
Albin Baby Peedieakkal <albinbaby26895@gmail.com>
2021-11-06 14:37:55 -0400

    spring initializer

---
+++ src/main/java/com/example/getmesocial2/Getmesocial2Application.java
@@ -0,0 +1,13 @@
+package com.example.getmesocial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Getmesocial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Getmesocial2Application.class, args);
+	}
+
+}
